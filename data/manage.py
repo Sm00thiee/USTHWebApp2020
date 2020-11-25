581c87ddbedfee1cc6d8232a1da4679c1ca7f451
Ntrug1 <quenpassroi1@gmail.com>
2020-11-25 23:25:19 +0700

    Update manage.py

--- data/manage.py
+++ data/manage.py
@@ -1,6 +1,8 @@
-from tools.crawler import CovidImageScraper
-from tools.addWebpage import add
+from .tools.crawler import CovidImageScraper
+from .tools.addWebpage import add
 from termcolor import colored
+
+
 def menu():
     print("""
     0. Quit
@@ -8,9 +10,10 @@ def menu():
     2. Crawl more picture list to links.txt
     """)
 
+
 def main():
     run = True
-    cmd_list = [0,1,2]
+    cmd_list = [0, 1, 2]
     while run:
         cmd = ""
         while cmd not in cmd_list:
@@ -19,7 +22,7 @@ def main():
             if cmd == 0:
                 run = False
                 break
-            elif cmd ==1:
+            elif cmd == 1:
                 web = input("Enter link want to add: ")
                 add(web)
             elif cmd == 2:
@@ -27,6 +30,7 @@ def main():
                 crawler.run()
             else:
                 print(colored("\n It's not a function.", "red"))
+
 
 if __name__ == "__main__":
     main()
